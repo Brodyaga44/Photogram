3df725410544e8d0a436ef6a4b434dfd8a63774f
Brodyaga44 <vdon_hiznyakov_dmitriy@mail.ru>
2024-09-02 15:59:09 +0300

    test

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,12 +1,9 @@
 import { TestFeature } from "@/features";
-import Logo from "@/shared/assets/react.svg?react";
 
 const Main = () => {
   return (
     <div>
-      <div>
-        <Logo />
-      </div>
+      <div></div>
       Main Page
       <TestFeature />
     </div>
